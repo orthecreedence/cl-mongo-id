a046e9de9204fa469216ac6fe8883c4f4110efe4
Andrew Danger Lyon <orthecreedence@gmail.com>
2012-04-24 10:41:54 -0700

    adding some optimizations (hardcoding values that are always computed the same, caching hostname, simplifying get-inc-val code (doest check timestamp anymore, just incs all the time)). also added oid-str function which converts a byte array id to a string

--- mongo-id.lisp
+++ mongo-id.lisp
@@ -1,21 +1,30 @@
 (defpackage :cl-mongo-id
   (:use :cl)
-  (:export oid)
+  (:export :oid :oid-str)
   (:nicknames :mongo-id))
 (in-package :cl-mongo-id)
 
 (defvar *id-inc* 0)
-(defvar *id-inc-timestamp* 0)
 (defvar *id-inc-lock* (bt:make-lock))
+
+(defvar *hostname* nil)
+(defvar *hostname-lock* (bt:make-lock))
 
 (defun oid (&optional id)
   "Generate a mongo id."
   (cond
-    ((stringp id)
-     (convert-hex-vector id))
+    ((stringp id) (convert-hex-vector id))
     ((vectorp id) id)
-    ((null id)
-     (create-new-id))))
+    ((null id)    (create-new-id))))
+
+(defun oid-str (id &key downcase)
+  "Given a vector ID, convert it to a string."
+  (let ((str ""))
+    (loop for byte across id do
+      (setf str (concatenate 'string str (format nil "~2,'0X" byte))))
+    (if downcase
+        (string-downcase str)
+        str)))
 
 (define-condition hex-conversion-error (error) ()
   (:report (lambda (o s)
@@ -35,10 +44,11 @@
 (defun create-new-id ()
   "Create a brand-spankin-new ObjectId using the current timestamp/inc values,
   along with hostname and process pid."
-  (let* ((hostname (get-hostname))
-         (pid (logand #xFFFF (get-current-pid)))
-         (timestamp (logand #xFFFFFFFF (get-current-timestamp)))
-         (inc (logand #xFFFFFF (get-inc-val timestamp))))
+  (declare (optimize (speed 3) (safety 1)))
+  (let ((hostname (get-hostname))
+        (pid (logand #xFFFF (get-current-pid)))
+        (timestamp (logand #xFFFFFFFF (get-current-timestamp)))
+        (inc (logand #xFFFFFF (get-inc-val))))
     (let ((hostname-bytes (subseq (md5:md5sum-sequence hostname) 0 3))
           (pid-bytes (convert-hex-vector (format nil "~4,'0X" pid)))
           (timestamp-bytes (convert-hex-vector (format nil "~8,'0X" timestamp)))
@@ -46,29 +56,33 @@
       (concatenate 'vector timestamp-bytes hostname-bytes pid-bytes inc-bytes))))
 
 (defun get-hostname ()
-  "Get hostname of machine."
-  (let ((host (machine-instance)))
-    (subseq host 0 (position #\  host))))
+  "Get hostname of machine (and cache it)."
+  (declare (optimize (speed 3) (safety 1)))
+  (bt:with-lock-held (*hostname-lock*)
+    (if *hostname*
+        *hostname*
+        (let ((host (machine-instance)))
+          (setf *hostname* (subseq host 0 (position #\  host)))
+          *hostname*))))
 
 (defun get-current-timestamp ()
   "Get current unix timestamp."
-  (- (get-universal-time) (encode-universal-time 0 0 0 1 1 1970 0)))
+  ;; 2208988800 == (encode-universal-time 0 0 0 1 1 1970 0)
+  (- (get-universal-time) 2208988800))
 
-(defun get-inc-val (&optional (timestamp (get-current-timestamp)))
+(defun get-inc-val ()
   "Thread-safe method to get current ObjectId inc value. Takes an optional
   timestamp value to calculate inc for."
+  (declare (optimize (speed 3) (safety 1)))
   (bt:with-lock-held (*id-inc-lock*)
-    (if (= timestamp *id-inc-timestamp*)
-        (incf *id-inc*)
-        (progn (setf *id-inc* 0)
-               (setf *id-inc-timestamp* timestamp)
-               0))))
+    (setf *id-inc* (logand #xFFFFFF (1+ *id-inc*)))
+    *id-inc*))
 
 (defun get-current-pid ()
   "Get the current process' PID. This function does it's best to be cross-
   implementation."
   #+clisp
-  (system:process-id)
+  (system::process-id)
   #+(and lispworks unix)
   (system::getpid)
   #+(and sbcl unix)
